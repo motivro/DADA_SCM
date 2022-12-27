822cff4139ca5501981329e8ad628d5b18ee1af5
motivro <motivro.official@gmail.com>
2022-12-27 10:32:49 +0900

    first commit

---
+++ admin/resources/js/ui.js
@@ -0,0 +1,40 @@
+
+$(document).ready(function(){
+    var accBox = $('#lnb .motiv_acc > li.nav_item');
+
+    for(i = 0; i < accBox.length; i++){
+        var acc = accBox.eq(i).children('.nav_menu');
+        for (j = 0; j < acc.length; j++) {
+            acc[j].addEventListener("click", function () {
+                // this.classList.toggle("active");
+                $(this).parent('li.nav_item').toggleClass('open');
+                // $(this).parent('li.nav_item').parent().parent().toggleClass('active');
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            });
+        }
+    }
+    
+    // var accBox2 = $('#lnb .motiv_acc > li.nav_item');
+
+    // for(i = 0; i < accBox2.length; i++){
+    //     var acc = accBox2.eq(i).children('.nav_menu');
+    //     for (j = 0; j < acc.length; j++) {
+    //         acc[j].addEventListener("click", function () {
+    //             // this.classList.toggle("active");
+    //             $(this).parent('li.nav_item').toggleClass('open');
+    //             // $(this).parent('li.nav_item').parent().parent().toggleClass('active');
+    //             var panel = this.nextElementSibling;
+    //             if (panel.style.maxHeight) {
+    //                 panel.style.maxHeight = null;
+    //             } else {
+    //                 panel.style.maxHeight = panel.scrollHeight + "px";
+    //             }
+    //         });
+    //     }
+    // }
+});
