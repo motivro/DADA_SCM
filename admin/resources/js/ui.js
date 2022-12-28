ee8548196daec0f458486f029ab53fb0da409067
motivro <motivro.official@gmail.com>
2022-12-28 16:20:30 +0900

    sidemenu ui 개발 배포

--- admin/resources/js/ui.js
+++ admin/resources/js/ui.js
@@ -1,7 +1,41 @@
 
 $(document).ready(function(){
-    var accBox = $('#lnb .motiv_acc > li.nav_item');
 
+    //사이드메뉴 추가
+    gitHubTgMenu();
+
+    if(location.href.indexOf('github') != -1){
+        githubUi.ini();
+    }
+});
+
+gitHubTgMenu = () => {
+    var locationOrigin = location.origin;
+    var serverSta = location.href.indexOf('github') != -1 ? '/DADA_SCM/admin/html/' : '/html/';
+    var pageRoute = location.origin + serverSta;
+
+    var sideMenuHtml = '';
+    sideMenuHtml += '<ul class="nav_list motiv_acc">';
+    $.each(PCADME, (index, item) => {
+        sideMenuHtml += '<li class="nav_item">';
+        var numBer = 0;
+        $.each( item, (s_index, s_item) => {
+            if(numBer == 0){
+                numBer++;
+                sideMenuHtml += '   <div class="nav_menu">' + s_item + '</div>';
+                sideMenuHtml += '   <ul class="submenu">';
+            } else {
+                sideMenuHtml += '<li class="sub_item"><a href="' + pageRoute + s_index + '.html">' + s_item + '</a></li>';
+            }
+        })
+        sideMenuHtml += '</ul>';
+        sideMenuHtml += '</li>';
+    })
+    sideMenuHtml += '</ul>';
+    $('.lnb_menu .motiv_acc').remove();
+    $('.lnb_menu').append(sideMenuHtml);
+
+    var accBox = $('#lnb .motiv_acc > li.nav_item');
     for(i = 0; i < accBox.length; i++){
         var acc = accBox.eq(i).children('.nav_menu');
         for (j = 0; j < acc.length; j++) {
@@ -18,35 +52,100 @@ $(document).ready(function(){
             });
         }
     }
-    
-    // var accBox2 = $('#lnb .motiv_acc > li.nav_item');
-
-    // for(i = 0; i < accBox2.length; i++){
-    //     var acc = accBox2.eq(i).children('.nav_menu');
-    //     for (j = 0; j < acc.length; j++) {
-    //         acc[j].addEventListener("click", function () {
-    //             // this.classList.toggle("active");
-    //             $(this).parent('li.nav_item').toggleClass('open');
-    //             // $(this).parent('li.nav_item').parent().parent().toggleClass('active');
-    //             var panel = this.nextElementSibling;
-    //             if (panel.style.maxHeight) {
-    //                 panel.style.maxHeight = null;
-    //             } else {
-    //                 panel.style.maxHeight = panel.scrollHeight + "px";
-    //             }
-    //         });
-    //     }
-    // }
-
-
-    if(location.href.indexOf('github') != -1){
-        githubUi.ini();
-    }
-});
+}
 
 var githubUi = {
     ini: () => {
-        console.log('github')
+        console.log('github');
     },
 
 }
+
+var PCADME = [
+    {
+        PCADME001: '상품관리',
+        PCADME001001: '상품목록',
+        PCADME001002: '일괄등록',
+    },
+    {
+        PCADME002: '다다딜관리',
+        PCADME002001: '상품관리',
+        PCADME002002: '판매요청',
+        PCADME002003: '판매현황',
+        PCADME002004: '판매종료',
+    },
+    {
+        PCADME003: '지정판매관리',
+        PCADME003001: '상품목록',
+    },
+    {
+        PCADME004: '판매상품관리',
+        PCADME004001: '전체상품목록',
+        PCADME004002: '소싱일반상품',
+        PCADME004003: '다다딜상품',
+        PCADME004004: '지정판매자상품',
+        PCADME004005: '쇼핑몰연계상품',
+    },
+    {
+        PCADME005: '강제종료관리',
+        PCADME005001: '강제종료상품관리',
+    },
+    {
+        PCADME006: '주문관리',
+        PCADME006001: '주문조회',
+        PCADME006002: '미결제확인',
+        PCADME006003: '발주확인/발송',
+        PCADME006004: '배송현황',
+        PCADME006005: '구매확정내역조회',
+        PCADME006006: '취소관리',
+        PCADME006007: '반품관리',
+        PCADME006008: '교환관리',
+    },
+    {
+        PCADME007: '정산관리',
+        PCADME007001: '정산내역조회',
+        PCADME007002: '정산지급현황',
+        PCADME007003: '매출증빙내용',
+    },
+    {
+        PCADME008: '고객센터',
+        PCADME008001: '고객센터',
+    },
+    {
+        PCADME009: '통계',
+        PCADME009001: '통계요약',
+        PCADME009002: '매출통계',
+        PCADME009003: '결제수단통계',
+        PCADME009004: '상품매출통계',
+        PCADME009005: '판매자판매통계',
+        PCADME009006: 'SCM플랫폼리포트',
+    },
+    {
+        PCADME010: '회원관리',
+        PCADME010001: '회원현황',
+        PCADME010002: '패널티관리',
+        PCADME010003: '휴면회원관리',
+        PCADME010004: '탈퇴회원',
+    },
+    {
+        PCADME011: '고객센터관리',
+        PCADME011001: '문의사항관리',
+        PCADME011002: '공지사항관리',
+    },
+    {
+        PCADME012: '기본정보관리',
+        PCADME012001: '회사정보관리',
+        PCADME012002: '약관/개인/금융관리',
+        PCADME012003: '운영자계정관리',
+        PCADME012004: '수수료설정',
+        PCADME012005: '소싱진열관리',
+    },
+    {
+        PCADME013: '쇼핑몰관리',
+        PCADME013001: '상품카테고리관리',
+        PCADME013002: '주소지관리',
+        PCADME013003: '금칙단어관리',
+        PCADME013004: '통합쇼핑몰',
+        PCADME013005: '로컬상가관리'
+    }
+]
