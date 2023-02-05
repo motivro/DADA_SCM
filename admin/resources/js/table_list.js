65333656fab30a430610ebdd89c720ec08216683
fun kim <>
2023-02-06 07:40:07 +0900

    테이블 리스트 추가 보완사항 완료

--- admin/resources/js/table_list.js
+++ admin/resources/js/table_list.js
@@ -130,6 +130,7 @@ var tFlexLock = {
         var reSize = 0;
         var scrollTableName = new Array(); 
         var reScrollRowSize = 0;
+        var firstSize = new Object();
         //일반 테이블 리스트를 재활용하기 위해 클래스를 추가합니다.
         $('#'+ obj.id +' .motiv_tbl').addClass('lock_tbl');
 
@@ -147,14 +148,12 @@ var tFlexLock = {
         .then(() => {
             //체크박스 넓이를 제외한 데이터 전체 넓이를 계산합니다.
             //hd_item에 해당 데이터 넓이를 추가합니다.
-            
             $.each( obj.tableSize, (name, size) => {
-                $('#'+ obj.id +' .motiv_tbl .hd_item[keyname='+ name +']').css('width', size +'px');
-                //전체 데이터 name을 배열에 저장합니다.
+                firstSize[name] = size;
                 scrollTableName.push(name);
                 obj.totalSize = obj.totalSize + size;
             });
-
+            
             //고정되는 리스트의 넓이를 구합니다.
             obj.scrollLookSize = tFlexLock.ckbSize;
             $.each( obj.scrollLook, (index, name) => {
@@ -166,16 +165,7 @@ var tFlexLock = {
                 }
             });
 
-            //item_row가 있을 경우 실행됨니다.
-            if($('#'+ obj.id).find('.item_row').length > 0){
-                //item_row 넓이값을 구하기 위해 필요한 값을 구합니다.
-                $.each( $('#'+ obj.id).find('.hd_item'), (index, target) => {
-                    var hdItem = $('#'+ obj.id).find('.hd_item').eq(index);
-                    if(hdItem.hasClass('item_row')){
-                        hdItem.css('width', tFlexLock.rowItemTh(hdItem, obj));
-                    }
-                });
-            }
+            
 
             //테이블의 전체 넓이를 구합니다.
             motivTblW = $('#'+ obj.id +' .motiv_tbl').width();
@@ -186,15 +176,12 @@ var tFlexLock = {
             if(($('#'+ obj.id).find('.tbl_inner').width() - obj.scrollLookSize) > scrollRowSize){
                 //데이터의 전체 넓이가 테이블의 넓이보다 작을 경우에 각 데이터의 넓이를 테이블의 넓이에 맞게 변경합니다.
                 //전체 데이터의 넓이를 다시 구하기 시작합니다.
-                
                 //테이블의 넓이와 데이터 넓이를 뺀 값에 데이터 갯수를 나눠 데이터 각각에 나눠주기 위한 값을 계산합니다.
                 reSize = (scrollRowBoxSize - scrollRowSize) / scrollTableName.length;
                 $.each( scrollTableName, (index, name) => {
-                    // console.log( obj.tableSize[name], index, name )
                     //전달받은 데이터 각각의 값에 reSize값을 더합니다.
                     obj.tableSize[name] = obj.tableSize[name] + reSize;
                     reScrollRowSize = reScrollRowSize + obj.tableSize[name];
-                    // console.log( index, obj.tableSize[name])
                 });
                 //다시 구한 전체 넓이로 값을 변경합니다.
             } else {
@@ -202,24 +189,47 @@ var tFlexLock = {
             }
         })
         .then(() => {
-            
+            //체크박스 넓이를 제외한 데이터 전체 넓이를 계산합니다.
+            //hd_item에 해당 데이터 넓이를 추가합니다.
+            $.each( obj.tableSize, (name, size) => {
+                $('#'+ obj.id +' .motiv_tbl .hd_item[keyname='+ name +']').css('width', size +'px');
+                //전체 데이터 name을 배열에 저장합니다.
+            });
+
+            //item_row가 있을 경우 실행됨니다.
+            if($('#'+ obj.id).find('.item_row').length > 0){
+                //item_row 넓이값을 구하기 위해 필요한 값을 구합니다.
+                $.each( $('#'+ obj.id).find('.hd_item'), (index, target) => {
+                    var hdItem = $('#'+ obj.id).find('.hd_item').eq(index);
+                    if(hdItem.hasClass('item_row')){
+                        hdItem.css('width', tFlexLock.rowItemTh(hdItem, obj.tableSize));
+                    }
+                });
+            }
+
             //look_body_head의 th영역은 고정되는 영역이라 동일 한값을 같습니다.
             $('#'+ obj.id +' .motiv_tbl').find('.look_body_head, .look_body_head .row_item').css('width', obj.scrollLookSize +'px');
             //스크롤 리스트 영역의 look_body_box 넓이는 테이블의 넓이에서 고정 테이블의 넓이를 뺀 값을 추가합니다.
             $('#'+ obj.id +' .motiv_tbl').find('.look_body_box').css('width', scrollRowBoxSize +'px');
-            //스크롤 리스트의 row_item은 고정되는 테이블의 데이터 넓이를 뺀 나머지 넓이값을 추가 합니다.
-            $('#'+ obj.id +' .motiv_tbl').find('.look_body_box .row_item').css('width', scrollRowSize +'px');
+
+            if(($('#'+ obj.id).find('.tbl_inner').width() - obj.scrollLookSize) > scrollRowSize){
+                //스크롤 리스트의 row_item은 고정되는 테이블의 데이터 넓이를 뺀 나머지 넓이값을 추가 합니다.
+                $('#'+ obj.id +' .motiv_tbl').find('.look_body_box .row_item').css('width', scrollRowBoxSize +'px');
+            } else {
+                //스크롤 리스트의 row_item은 고정되는 테이블의 데이터 넓이를 뺀 나머지 넓이값을 추가 합니다.
+                $('#'+ obj.id +' .motiv_tbl').find('.look_body_box .row_item').css('width', scrollRowSize +'px');
+            }
 
             //lnb 이벤트가 발생할 경우 테이블의 넓이조정이 필요할때 구동될 이벤트 입니다.
             //주 대상은 데이터 전체 넓이가 데이블 넓이보다 작을 경우에 주 대상입니다.
             const $target = document.querySelector('#'+ obj.id +' .tbl_inner');
             const observer = new ResizeObserver((entries) => {
-                tFlexLock.reSizeChange(obj, scrollTableName, reScrollRowSize);
+                tFlexLock.reSizeChange(obj, scrollTableName, reScrollRowSize, scrollRowSize, firstSize);
             });
             observer.observe($target);  // 크기변화를 관찰할 요소지정
 
             $(window).on('resize', function(){
-                tFlexLock.reInit(obj);
+                // tFlexLock.reInit(obj);
             });
         })
         
@@ -227,34 +237,54 @@ var tFlexLock = {
 
     //리사이즈 및 lnb 이벤트가 발생할때 값을 다시 구합니다.
     //함수 내용은 위와 동일 합니다.
-    reSizeChange: (obj, scrollTableName, scrollRowSize) => {
-        // console.log('reSizeChange', ($('#'+ obj.id).find('.tbl_inner').width() - obj.scrollLookSize) > scrollRowSize)
+    reSizeChange: (obj, scrollTableName, reScrollRowSize, rowSize,firstSize) => {
         var motivTblW = $('#'+ obj.id +' .motiv_tbl').width();
-        var scrollRowBoxSize = motivTblW - obj.scrollLookSize;
+        var scrollRowSize = obj.totalSize - obj.scrollLookSize;
+        var scrollRowBoxSize = (motivTblW - obj.scrollLookSize) > rowSize ? (motivTblW - obj.scrollLookSize) : rowSize;
         var reSize = 0;//사이즈를 다시 체크하기 위한 여비 변수 입니다.
         var reScrollRowSize = 0;
         
-        $.each( obj.tableSize, (name, size) => {
-            $('#'+ obj.id +' .motiv_tbl .hd_item[keyname='+ name +']').css('width', size +'px');
-            //전체 데이터 name을 배열에 저장합니다.
-            obj.totalSize = obj.totalSize + size;
-        });
-
-        //item_row가 있을 경우 실행됨니다.
-        if($('#'+ obj.id).find('.item_row').length > 0){
-            //item_row 넓이값을 구하기 위해 필요한 값을 구합니다.
-            $.each( $('#'+ obj.id).find('.hd_item'), (index, target) => {
-                var hdItem = $('#'+ obj.id).find('.hd_item').eq(index);
-                if(hdItem.hasClass('item_row')){
-                    hdItem.css('width', tFlexLock.rowItemTh(hdItem, obj));
-                }
+        if(($('#'+ obj.id).find('.tbl_inner').width() - obj.scrollLookSize) > scrollRowSize){
+            //데이터의 전체 넓이가 테이블의 넓이보다 작을 경우에 각 데이터의 넓이를 테이블의 넓이에 맞게 변경합니다.
+            //전체 데이터의 넓이를 다시 구하기 시작합니다.
+            //테이블의 넓이와 데이터 넓이를 뺀 값에 데이터 갯수를 나눠 데이터 각각에 나눠주기 위한 값을 계산합니다.
+            reSize = (scrollRowBoxSize - scrollRowSize) / scrollTableName.length;
+            console.log(reSize)
+            $.each( scrollTableName, (index, name) => {
+                //전달받은 데이터 각각의 값에 reSize값을 더합니다.
+                firstSize[name] = obj.tableSize[name] + reSize;
+                reScrollRowSize = reScrollRowSize + firstSize[name];
             });
+            //다시 구한 전체 넓이로 값을 변경합니다.
+        } else {
+            reScrollRowSize = scrollRowSize;
         }
 
+        if((motivTblW - obj.scrollLookSize) > rowSize){
+            //체크박스 넓이를 제외한 데이터 전체 넓이를 계산합니다.
+            //hd_item에 해당 데이터 넓이를 추가합니다.
+            $.each( firstSize, (name, size) => {
+                $('#'+ obj.id +' .motiv_tbl .hd_item[keyname='+ name +']').css('width', size +'px');
+                //전체 데이터 name을 배열에 저장합니다.
+            });
+
+            //item_row가 있을 경우 실행됨니다.
+            if($('#'+ obj.id).find('.item_row').length > 0){
+                //item_row 넓이값을 구하기 위해 필요한 값을 구합니다.
+                $.each( $('#'+ obj.id).find('.hd_item'), (index, target) => {
+                    var hdItem = $('#'+ obj.id).find('.hd_item').eq(index);
+                    if(hdItem.hasClass('item_row')){
+                        hdItem.css('width', tFlexLock.rowItemTh(hdItem, firstSize));
+                    }
+                });
+            }
+        }
+
+        //look_body_head의 th영역은 고정되는 영역이라 동일 한값을 같습니다.
+        $('#'+ obj.id +' .motiv_tbl').find('.look_body_head, .look_body_head .row_item').css('width', obj.scrollLookSize +'px');
+        //스크롤 리스트 영역의 look_body_box 넓이는 테이블의 넓이에서 고정 테이블의 넓이를 뺀 값을 추가합니다.
         $('#'+ obj.id +' .motiv_tbl').find('.look_body_box').css('width', scrollRowBoxSize +'px');
-        //스크롤 리스트의 row_item은 고정되는 테이블의 데이터 넓이를 뺀 나머지 넓이값을 추가 합니다.
-        $('#'+ obj.id +' .motiv_tbl').find('.look_body_box .row_item').css('width', scrollRowSize +'px');
-        
+        $('#'+ obj.id +' .motiv_tbl').find('.look_body_box .row_item').css('width', scrollRowBoxSize +'px');
     },
 
 
@@ -277,11 +307,10 @@ var tFlexLock = {
     },
 
     rowItemTh: (target, obj) => {
-        
         var itemThSize = 0;
         var targetCh = target.children('.item_row_tbl').children('div');
         $.each(  targetCh, (index, size) => {
-            var keynameSize = obj.tableSize[targetCh.eq(index).attr('keyname')];
+            var keynameSize = obj[targetCh.eq(index).attr('keyname')];
             targetCh.eq(index).css('width', keynameSize);
             itemThSize = itemThSize + keynameSize;
         });
